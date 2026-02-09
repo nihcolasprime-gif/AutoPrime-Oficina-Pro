c2a03a93e096856337948fb0f087138cf87edcff
nihcolasprime-gif <nihcolasprime@gmail.com>
2026-02-08 22:12:27 -0300

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useAutoPrime } from '../context/AutoPrimeContext';
+import { useAutoPrime } from '../scr/context/AutoPrimeContext';
 import { LayoutDashboard, Users, Car, Package, Wrench, Wallet, BellRing } from 'lucide-react';
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
