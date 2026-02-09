13dc45fc6378f0d9f911bd12ca6e13e63251b641
nihcolasprime-gif <nihcolasprime@gmail.com>
2026-02-08 22:24:18 -0300

    Update Settings.tsx

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import { useAutoPrime } from '../scr/context/AutoPrimeContext';
+import { useAutoPrime } from '../src/context/AutoPrimeContext';
 import { BellRing, Calendar, Save, Car, AlertTriangle, CheckCircle, Clock, Search, ArrowRight } from 'lucide-react';
 
 export const Settings = () => {
