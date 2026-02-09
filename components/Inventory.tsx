ed9b36b1a5035e1b1aa1737c09e4205be37f0cc2
nihcolasprime-gif <nihcolasprime@gmail.com>
2026-02-08 22:49:03 -0300

    Update Inventory.tsx

--- components/Inventory.tsx
+++ components/Inventory.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import { useAutoPrime } from '../scr/context/AutoPrimeContext';
+import { useAutoPrime } from '../src/lib/AutoPrimeContext';
 import { Modal } from './ui/Modal';
 import { Plus, Edit2, Trash2, Package, AlertTriangle, Search, TrendingUp, AlertCircle, Box } from 'lucide-react';
 import { Part } from '../types';
